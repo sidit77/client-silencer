5fdd67e48267349681b00ffb2d021aeef201036b
sidit77 <sidit77@gmail.com>
2023-05-14 22:08:58 +0200

    fixed the hooking

--- client-hook/src/lib.rs
+++ client-hook/src/lib.rs
@@ -1,24 +1,107 @@
+mod hook_old;
+
 use std::ffi::c_void;
+use std::iter::once;
+use std::ptr::{null, null_mut};
+use anyhow::ensure;
+use detour3::RawDetour;
+use once_cell::sync::OnceCell;
+use windows_sys::{s, w};
+use windows_sys::Win32::Foundation::{BOOL, FALSE, HMODULE, HWND, TRUE};
+use windows_sys::Win32::System::LibraryLoader::{DisableThreadLibraryCalls, GetModuleHandleW, GetProcAddress};
+use windows_sys::Win32::System::SystemServices::*;
+use windows_sys::Win32::System::Threading::CreateThread;
+use windows_sys::Win32::UI::WindowsAndMessaging::{MB_OK, MessageBoxW, SET_WINDOW_POS_FLAGS};
+
+#[no_mangle]
+pub unsafe extern "stdcall" fn DllMain(hmodule: HMODULE, reason: u32, _: *mut c_void) -> BOOL {
+    match reason {
+        DLL_PROCESS_ATTACH => {
+            DisableThreadLibraryCalls(hmodule);
+            let result = CreateThread(
+                null(),
+                0,
+                Some(attachment_thread),
+                null(),
+                0,
+                null_mut()
+            );
+            if result == 0 {
+                return FALSE;
+            }
+        },
+        DLL_PROCESS_DETACH => { },
+        _ => { }
+    }
+    TRUE
+}
+
+type WinPosSig = extern "system" fn(HWND, HWND, i32, i32, i32, i32, SET_WINDOW_POS_FLAGS) -> BOOL;
+static HOOK: OnceCell<RawDetour> = OnceCell::new();
+
+#[allow(non_snake_case)]
+unsafe extern "system" fn SetWindowPos(hwnd: HWND, _hwndinsertafter: HWND, x: i32, y: i32, cx: i32, cy: i32, uflags: SET_WINDOW_POS_FLAGS) -> BOOL {
+    let original: WinPosSig = std::mem::transmute(HOOK.get_unchecked().trampoline());
+    original(hwnd, 0, x, y, cx, cy, uflags)
+}
+
+unsafe fn hook() -> anyhow::Result<()> {
+    let user32_lib = GetModuleHandleW(w!("user32.dll"));
+    ensure!(user32_lib != 0);
+
+    let set_window_pos = GetProcAddress(user32_lib, s!("SetWindowPos"));
+    ensure!(set_window_pos.is_some());
+    let set_window_pos = set_window_pos.unwrap();
+
+    let offset = (((SetWindowPos as *mut isize) as isize - (set_window_pos as *mut isize) as isize) - 5) as usize;
+    println!("{}", offset);
+
+    HOOK.get_or_try_init(|| RawDetour::new(set_window_pos as *const (), SetWindowPos as *const ()))?.enable()?;
+    Ok(())
+}
+
+unsafe extern "system" fn attachment_thread(_lpthreadparameter: *mut c_void) -> u32 {
+    let result = format!("{:?}", hook())
+        .encode_utf16()
+        .chain(once(0u16))
+        .collect::<Vec<u16>>();
+    MessageBoxW(0, result.as_ptr(), w!("Hook result"), MB_OK);
+    0
+}
+
+/*
+use std::ffi::c_void;
+use std::ptr::{null, null_mut};
 use anyhow::ensure;
 use detour3::{RawDetour};
 use once_cell::sync::OnceCell;
 use windows_sys::{s, w};
 use windows_sys::Win32::Foundation::{BOOL, HMODULE, HWND};
-use windows_sys::Win32::System::LibraryLoader::{GetModuleHandleW, GetProcAddress};
+use windows_sys::Win32::System::LibraryLoader::{DisableThreadLibraryCalls, GetModuleHandleW, GetProcAddress};
 use windows_sys::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
-use windows_sys::Win32::UI::WindowsAndMessaging::SET_WINDOW_POS_FLAGS;
+use windows_sys::Win32::System::Threading::CreateThread;
+use windows_sys::Win32::UI::WindowsAndMessaging::{SET_WINDOW_POS_FLAGS, SWP_NOZORDER};
 
 #[no_mangle]
-pub extern "stdcall" fn DllMain(_hinst_dll: HMODULE, fdw_reason: u32, _lpv_reserved:  *mut c_void) -> i32 {
+pub extern "stdcall" fn DllMain(hinst_dll: HMODULE, fdw_reason: u32, _lpv_reserved:  *mut c_void) -> i32 {
     match fdw_reason {
+        // The .dll has been loaded
         DLL_PROCESS_ATTACH => {
             unsafe {
-                if let Err(err) = hook() {
-                    let _ = std::fs::write("C:\\client-hook.log", format!("{:?}", err));
-                }
+                DisableThreadLibraryCalls(hinst_dll);
+                // Create a thread that executes our dll_attach function
+                CreateThread(
+                    null(),
+                    0,
+                    Some(dll_process_attach_event),
+                    hinst_dll as _,
+                    0,
+                    null_mut(),
+                );
             }
             return 1i32;
         }
+        // ignore for now
         _ => 1i32,
     }
 }
@@ -26,7 +109,7 @@ pub extern "stdcall" fn DllMain(_hinst_dll: HMODULE, fdw_reason: u32, _lpv_reser
 type WinPosSig = extern "system" fn(HWND, HWND, i32, i32, i32, i32, SET_WINDOW_POS_FLAGS) -> BOOL;
 static HOOK: OnceCell<RawDetour> = OnceCell::new();
 
-unsafe extern "system" fn set_window_pos_detour(hwnd: HWND, _hwndinsertafter: HWND, x: i32, y: i32, cx: i32, cy: i32, uflags: SET_WINDOW_POS_FLAGS) -> BOOL {
+unsafe extern "system" fn SetWindowPos(hwnd: HWND, hwndinsertafter: HWND, x: i32, y: i32, cx: i32, cy: i32, uflags: SET_WINDOW_POS_FLAGS) -> BOOL {
     let original: WinPosSig = std::mem::transmute(HOOK.get_unchecked().trampoline());
     original(hwnd, 0, x, y, cx, cy, uflags)
 }
@@ -44,6 +127,8 @@ unsafe fn hook() -> anyhow::Result<()> {
     ensure!(set_window_pos.is_some());
     let set_window_pos = set_window_pos.unwrap();
 
-    HOOK.get_or_try_init(|| RawDetour::new(set_window_pos as *const (), set_window_pos_detour as *const ()))?.enable()?;
+    HOOK.get_or_try_init(|| RawDetour::new(set_window_pos as *const (), SetWindowPos as *const ()))?.enable()?;
     Ok(())
 }
+
+ */
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::env::current_exe;
+use std::ffi::c_void;
 use std::mem::size_of_val;
-use std::ptr::{null, null_mut};
+use std::ptr::null_mut;
 use sysinfo::{PidExt, ProcessExt, SystemExt};
-use widestring::{U16CString};
-use windows_sys::{s, w};
-use windows_sys::Win32::System::Diagnostics::Debug::WriteProcessMemory;
-use windows_sys::Win32::System::LibraryLoader::{GetModuleHandleW, GetProcAddress};
-use windows_sys::Win32::System::Memory::{MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE, VirtualAllocEx};
-use windows_sys::Win32::System::Threading::*;
+use widestring::{U16CString, U16String};
+use windows::imp::GetProcAddress;
+use windows::{s, w};
+use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
+use windows::Win32::System::LibraryLoader::GetModuleHandleW;
+use windows::Win32::System::Memory::{MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE, VirtualAllocEx};
+use windows::Win32::System::Threading::*;
 
 fn main() {
     unsafe {
@@ -21,51 +23,49 @@ fn main() {
                 PROCESS_VM_OPERATION |
                 PROCESS_VM_WRITE |
                 PROCESS_VM_READ,
-            0, pid
-        );
-        assert_ne!(process_handle, 0, "Failed to open process");
+            false, pid
+        ).expect("Failed to open process");
 
-        let kernel32 = GetModuleHandleW(w!("kernel32.dll"));
-        assert_ne!(kernel32, 0, "Failed to get handle for kernel32.dll");
-        let load_library = GetProcAddress(kernel32, s!("LoadLibraryW"))
-            .expect("Failed to the address of LoadLibraryW");
-        let mut dll_path = current_exe()
-            .expect("Failed to get exe path");
+        let kernel32 = GetModuleHandleW(w!("kernel32.dll")).unwrap();
+        let load_library = GetProcAddress(kernel32.0, s!("LoadLibraryW"));
+        println!("{:?}", load_library);
+        assert!(!load_library.is_null());
+        let mut dll_path = current_exe().unwrap();
         dll_path.pop();
         dll_path.push("client_hook.dll");
-        println!("Hook path: {}", dll_path.display());
-        assert!(dll_path.exists(), "Path the the hook dll is wrong");
-        let dll_path =  U16CString::from_os_str(&dll_path.into_os_string())
-            .expect("Can not convert path into c string");
+        assert!(dll_path.exists());
+        let dll_path =  U16CString::from_os_str(&dll_path.into_os_string()).unwrap();
+        println!("{} ({})", dll_path.display(), dll_path.len());
         let dll_path = dll_path.as_slice_with_nul();
+        println!("{}", size_of_val(dll_path));
 
         let virtual_alloc_ptr = VirtualAllocEx(
             process_handle,
-            null(),
+            None,
             size_of_val(dll_path),
             MEM_COMMIT | MEM_RESERVE,
             PAGE_READWRITE);
-        assert!(!virtual_alloc_ptr.is_null(), "Failed to allocate memory in the target process");
+        println!("{:?}", virtual_alloc_ptr);
+        assert!(!virtual_alloc_ptr.is_null());
 
 
-        assert_ne!(WriteProcessMemory(
+        WriteProcessMemory(
             process_handle,
             virtual_alloc_ptr,
             dll_path.as_ptr() as _,
             size_of_val(dll_path),
-            null_mut()
-        ), 0, "Failed to write path to memory");
+            None
+        ).unwrap();
 
-        assert_ne!(CreateRemoteThread(
+        CreateRemoteThread(
             process_handle,
-            null(),
+            None,
             0,
             Some(std::mem::transmute(load_library)),
-            virtual_alloc_ptr,
+            Some(virtual_alloc_ptr),
             0,
-            null_mut()
-        ), 0, "Failed to insert hook");
+            None
+        ).unwrap();
 
-        println!("Hook installed!");
     }
 }
